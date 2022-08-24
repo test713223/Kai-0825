5dd67524b275f34b475ed208e4e66a902afd01cc
test713223 <qoo654ooq@gmail.com>
2022-08-25 04:08:26 +0800

    new project set Pom.xnl

---
+++ src/main/java/com/xxbank/shop0825/Shop0825Application.java
@@ -0,0 +1,13 @@
+package com.xxbank.shop0825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop0825Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop0825Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/xxbank/shop0825/Shop0825ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxbank.shop0825;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop0825ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
